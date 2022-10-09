870ff22d1567c76710653b1585a610c58e96747f
Sundar-Lakshmi <sundaralakshmipremi24@gmail.com>
2022-10-09 18:51:53 +0530

    Add the data.txt file

--- Stream/src/Stream.java
+++ Stream/src/Stream.java
@@ -1,13 +1,20 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
 public class Stream {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         List num = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
 
-        num.stream()
+        List<String> file = Files.readAllLines(Paths.get("data.txt"));
+
+//        file.forEach(System.out::println);
+
+        file.stream()
                 .skip(1)
-                .limit(7)
-                .forEach(obj -> System.out.print(obj));
+                .limit(5)
+                .forEach(System.out::println);
     }
 }
